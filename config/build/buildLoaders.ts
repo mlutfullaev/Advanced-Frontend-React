eba0e7c2fe13ad79e0535c663857419fe34c14f7
magalutfullaev <mhoja9494@gmail.com>
2023-12-30 11:48:48 +0500

    add: Css to WebpackConfgi

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,25 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+  const cssLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:8]' : 'hash:base64:8',
+          },
+        }
+      },
+      "sass-loader",
+    ],
+  }
   const tsLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
@@ -10,5 +28,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
   return [
     tsLoader,
+    cssLoaders
   ]
 }
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+      adf
+    </div>
+  );
+};
+
+export default App;
