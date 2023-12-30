b85ece15e6586b81bad46b738d8a6b6fea6c1202
magalutfullaev <mhoja9494@gmail.com>
2023-12-30 11:11:48 +0500

    DevServer for Webpack

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -4,22 +4,25 @@ import path from "path";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-  const {paths, mode} = options;
+  const {paths, mode, isDev} = options;
 
   return {
     mode,
     entry: paths.entry,
     output: {
       filename: '[name].[contenthash].js',
-      path: path.resolve(__dirname, 'build'),
+      path: paths.build,
       clean: true,
     },
+    devtool: isDev ? 'inline-source-map' : undefined,
     plugins: buildPlugins(options),
     module: {
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devServer: isDev ? buildDevServer(options) : undefined
   }
 }
